490c513b167789eba9d561a8f1e69a80e00ad947
Moria Rozenfeld <mrozenfe@acad.jct.ac.il>
2024-05-19 10:54:02 +0300

    start stage 2

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,34 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Unit tests for primitives.Point class
+ */
+public class PointTest {
+    /**
+     * Test method for {@link Vector#subtract(Point)}
+     */
+    @Test
+    public void testSubtract() {
+    }
+    /**
+     * Test method for {@link Point#add(Vector)}.
+     */
+    @Test
+    public void testAdd() {
+    }
+    /**
+     * Test method for {@link Double#(primitives.Point)}.
+     */
+    @Test
+    public void testDistanceSquared() {
+    }
+    /**
+     * Test method for {@link Double#distance(Point)}.
+     */
+    @Test
+    public void testDistance() {
+    }
+}
